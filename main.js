28caeeb6118094f4bd9dd02c2dd513bd0d3a7643
MindHunter <Scofield89@yandex.ru>
2022-03-08 20:48:06 +0300

    Не уверен что верно всё сделал, но как смог

--- main.js
+++ main.js
@@ -7,16 +7,16 @@ const products = [
     {id: 4, title: 'Gamepad', price: 50},
 ];
 
-const renderProduct = (title, price) =>
+const renderProduct = (item) =>
 `<div class="product-item">
-    <img class = "product-photo" src="zaglushka.jpg" alt="product_photo">
-    <h3>${title}</h3>
-    <p>${price} руб.</p>
+    <img class = "product-photo" src="${item.id}.jpg" alt="product_photo">
+    <h3>${item.title}</h3>
+    <p>${item.price} руб.</p>
     <button class="buy-btn">Купить</button>
 </div>`;
 
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
+    const productsList = list.map(item => renderProduct(item));
     console.log (productsList);
     document.querySelector('.products').innerHTML = productsList.join('');
 };

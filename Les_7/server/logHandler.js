39bc44c4b7a64d5738ce5ecc649fc5fca92666ca
MindHunter <Scofield89@yandex.ru>
2022-05-03 20:48:45 +0300

    Вроде закончил ДЗ 7, и даже всё работает, но надо подправить вёрстку корзины

--- Les_7/server/logHandler.js
+++ Les_7/server/logHandler.js
@@ -1,5 +1,5 @@
-const moment = require('moment');
 const fs = require('fs');
+const moment = require('moment');
 
 const logs = {
     add: 'Added to cart',
@@ -12,25 +12,25 @@ let addLog = (log, cartLog) => {
     return JSON.stringify(log, null, 4);
 };
 
-const logHandler = ( req, res, action, file ) => {
-    fs.readFile( file, 'utf8', ( err, data ) => {
-        if ( err ) {
-            res.sendStatus( 404, JSON.stringify( { result: 0, text: err } ) );
+const logHandler = (req, res, action, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            res.sendStatus(404, JSON.stringify({ result: 0, text: err }));
         } else {
             let cartLog = {
                 Product_name: req.params.name,
-                Action: logs[ action ],
+                Action: logs[action],
                 Time: `${ moment().format( 'MMMM Do YYYY, h:mm:ss a' ) }`
             };
-            let newLog = addLog( JSON.parse( data ), cartLog );
-            fs.writeFile( file, newLog, ( err ) => {
-                if ( err ) {
+            let newLog = addLog(JSON.parse(data), cartLog);
+            fs.writeFile(file, newLog, (err) => {
+                if (err) {
                     console.log('Error');
                 } else {
                     console.log('Success');
                 }
-            } );
+            });
         }
-    } )
+    })
 };
 module.exports = logHandler;
